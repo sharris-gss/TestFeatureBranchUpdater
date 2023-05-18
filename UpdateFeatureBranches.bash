512e3fa23054c55818d4358dc51c3e92001aa05c
Scott Harris <sharris@gssmail.com>
2023-05-18 09:24:31 -0500

    Update UpdateFeatureBranches.bash

--- UpdateFeatureBranches.bash
+++ UpdateFeatureBranches.bash
@@ -3,11 +3,11 @@ branchDefiningCommit="cf07f42"
 
 devBranchName="dev"
 
-$(git checkout $devBranchName)
-$(git merge origin/$releaseBranchName)
-$(git push origin $devBranchName)
+( git checkout $devBranchName )
+( git merge origin/$releaseBranchName )
+( git push origin $devBranchName )
 
-branches=$(git branch --contains $branchDefiningCommit | tr '* ' ' ')
+branches=( git branch --contains $branchDefiningCommit | tr '* ' ' ' )
 branches=( $branches )
 
 for i in "${branches[@]}"
@@ -19,17 +19,17 @@ do
 			echo "$i is the development branch, already updated"
 		else
 			echo "Updating branch $i with changes from $devBranchName"
-			$(git checkout $i)
+			( git checkout $i )
 			
-			if $(git merge origin/$devBranchName); then
+			if (git merge origin/$devBranchName); then
 				echo "Merge Success"
 			else
 				echo "Merge Failure"
-				$( git reset --hard )
-				$( git clean -fxd )	
+				( git reset --hard )
+				( git clean -fxd )	
 			fi
 
-			$(git push origin $i)
+			( git push origin $i )
 		fi
 	fi
 done
