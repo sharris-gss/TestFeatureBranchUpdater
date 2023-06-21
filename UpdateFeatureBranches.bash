995a7dbb9208b126792ca4c84a8acf22c606176e
Scott Harris <sharris@gssmail.com>
2023-06-21 08:31:35 -0500

    Test refactoring and changing of commands to ${}

--- UpdateFeatureBranches.bash
+++ UpdateFeatureBranches.bash
@@ -3,11 +3,26 @@ branchDefiningCommit="cf07f42"
 
 devBranchName="dev"
 
-output=$( git checkout $devBranchName )
-output+=$( git merge origin/$releaseBranchName -m "Merge $releaseBranchName into $devBranchName" )
-output+=$( git push origin $devBranchName )
+checkout_and_update_git_branch () {
+	${ git checkout $0 }
+	${ git merge origin/$devBranchName -m "Merge $devBranchName into $0" }
+	statusCode=$?
+}
 
-branches=$( git branch --contains $branchDefiningCommit | tr '* ' ' ' )
+reset_git_checkout () {
+	${ git reset --hard }
+	${ git clean -fxd }
+}
+
+update_dev_branch () {
+	${ git checkout $devBranchName }
+	${ git merge origin/$releaseBranchName -m "Merge $releaseBranchName into $devBranchName" }
+	${ git push origin $devBranchName }
+}
+
+update_dev_branch
+
+branches=${ git branch --contains $branchDefiningCommit | tr '* ' ' ' }
 branches=( $branches )
 
 for i in "${branches[@]}"
@@ -19,21 +34,19 @@ do
 			echo "$i is the development branch, already updated"
 		else
 			echo "Updating branch $i with changes from $devBranchName"
-			output+=$( git checkout $i )
-			output+=$( git merge origin/$devBranchName -m "Merge $devBranchName into $i" )
 
-			statusCode=$?
+			checkout_and_update_git_branch "$i"
 
 			if [ $statusCode -eq 0 ]
 			then
 				echo "Merge Success"
 			else
 				echo "Merge Failure"
-				output+=$( git reset --hard )
-				output+=$( git clean -fxd )	
+				
+				reset_git_checkout
 			fi
 
-			output+=$( git push origin $i )
+			output+=${ git push origin $i }
 		fi
 	fi
 done
