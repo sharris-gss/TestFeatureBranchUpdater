ca93a5f217bc81bffee5f0f5c47f9508fee566f5
Scott Harris <sharris@gssmail.com>
2023-06-21 12:38:16 -0500

    Update Email logic to print the information out in the console

--- UpdateFeatureBranches.bash
+++ UpdateFeatureBranches.bash
@@ -1,3 +1,4 @@
+repository_name="TestFeatureBranchUpdater"
 releaseBranchName="main"
 branchDefiningCommit="cf07f42"
 devBranchName="dev"
@@ -37,12 +38,20 @@ get_gss_email_from_github_name () {
 
 # email_user (email_address, email_subject, email_message)
 email_user () {
-	echo ""
+	echo "Email Address: $1"
+	echo "Subject: $2"
+	echo "Body: $3"
 }
 
 # email_gss_user_to_resolve_conflicts (github_user, branch_name)
 email_gss_user_to_resolve_conflicts () {
-	echo $( get_gss_email_from_github_name "$1" )
+	echo ""
+	local gss_email=$( get_gss_email_from_github_name "$1" )
+	local email_subject="Automatic Update Failed on branch $2"
+	local email_message="Automatic update of branch $2 in the $repository_name repository failed. Resolve any conflicts and then update this branch manually from $devBranchName"
+
+	email_user "$gss_email" "$email_subject" "$email_message"
+	echo ""
 }
 
 echo "Updating development branch"
@@ -73,7 +82,7 @@ do
 				reset_git_checkout > /dev/null 2>&1
 
 				# Email Associated User
-				email_gss_user_to_resolve_conflicts "sharris-gss" ""
+				email_gss_user_to_resolve_conflicts "sharris-gss" "$i"
 			fi
 		fi
 	fi
