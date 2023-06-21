29630c6d342a24948fec14673207c093345f9994
Scott Harris <sharris@gssmail.com>
2023-06-21 15:16:00 -0500

    Add reading of github issues

--- UpdateFeatureBranches.bash
+++ UpdateFeatureBranches.bash
@@ -2,10 +2,48 @@ repository_name="TestFeatureBranchUpdater"
 releaseBranchName="main"
 branchDefiningCommit="cf07f42"
 devBranchName="dev"
+releaseVersion="2023.1"
 
 get_all_related_branches () {
 	branches=$( git branch --contains $branchDefiningCommit | tr '* ' ' ' )
 	branches=( $branches )
+}
+
+get_all_in_testing_relevant_issues () {
+	# get list of all issues
+	local raw_issues=$(gh issue list --label "In Testing" --label "$releaseVersion")
+
+	# split issues by line and take first element
+	local oldIFS=$IFS
+	IFS=$'\n' issueArray=( $raw_issues )
+	IFS=$oldIFS
+
+	# concatenate the issue #s together
+	local retIssue=""
+	
+	for issue in "${issueArray[@]}"
+	do
+		local currIssue=( $issue )
+		retIssue+="${currIssue[0]} "
+	done
+	echo $retIssue
+}
+
+get_all_branches_to_update_from_issues () {
+	# get all relevant issues
+	local issues=$( get_all_in_testing_relevant_issues )
+	# convert string into array
+	local issueArray=( $issues )
+	# for each issue in the array, get associated branches & concatenate to a string
+
+	local retBranches=""
+
+	for issue in "${issueArray[@]}"
+	do
+		retBranches+="$(gh issue develop --list $issue) "
+	done
+
+	echo $retBranches
 }
 
 # checkout_and_update_git_branch (branch_name)
@@ -58,7 +96,9 @@ echo "Updating development branch"
 update_dev_branch > /dev/null 2>&1
 
 echo "Getting all related branches"
-get_all_related_branches > /dev/null 2>&1
+# get_all_related_branches > /dev/null 2>&1
+branches=$(get_all_branches_to_update_from_issues)
+branches=( $branches )
 
 for i in "${branches[@]}"
 do
@@ -71,7 +111,7 @@ do
 			echo "$i is the development branch, already updated"
 		else
 			echo "Updating branch $i with changes from $devBranchName"
-			checkout_and_update_git_branch "$i" > /dev/null 2>&1
+			checkout_and_update_git_branch "$i" #> /dev/null 2>&1
 
 			if [ $statusCode -eq 0 ]
 			then
